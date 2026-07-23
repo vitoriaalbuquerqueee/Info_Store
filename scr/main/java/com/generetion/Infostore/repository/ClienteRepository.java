5f7f01ada6c5207bc56bf451db0853341e1a954a
Vitória Albuquerque <146150232+vitoriaalbuquerqueee@users.noreply.github.com>
2026-07-22 23:16:00 -0300

    Update ClienteRepository.java

--- scr/main/java/com/generetion/Infostore/repository/ClienteRepository.java
+++ scr/main/java/com/generetion/Infostore/repository/ClienteRepository.java
@@ -1,1 +1,15 @@
-bwbahabbwbw
+package com.generation.infostore.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.infostore.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+    public List<Cliente> findAllByNomeContainingIgnoreCase(String nome);
+
+}
+
